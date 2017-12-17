f540b5948362631379c7503b39ed4ef81882e3d9
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2017-12-17 16:39:15 +0100

    first commit

--- mdd/src/main/java/io/mateu/ui/mdd/client/MDDMenu.java
+++ mdd/src/main/java/io/mateu/ui/mdd/client/MDDMenu.java
@@ -13,7 +13,7 @@ public class MDDMenu extends AbstractMenu {
     public MDDMenu(Object... args) {
         super((args.length > 0)?"" + args[0]:"Missing parameters at MDDMenu");
 
-        for (int pos = 1; pos < args.length; pos++) {
+        for (int pos = 2; pos < args.length; pos++) {
             if ((pos + 1) % 2 == 0) {
                 if (args[pos - 1] instanceof String && args[pos] instanceof Class) {
                     String n = (String) args[pos - 1];
