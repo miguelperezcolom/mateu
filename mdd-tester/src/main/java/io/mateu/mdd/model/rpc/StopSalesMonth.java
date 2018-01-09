e849c3e38b7294cd4de534b36e63ea109362e721
miguelperezcolom <miguelperezcolom@gmail.com>
2018-01-09 12:47:25 +0100

    xx

--- mdd-tester/src/main/java/io/mateu/mdd/model/rpc/StopSalesMonth.java
+++ mdd-tester/src/main/java/io/mateu/mdd/model/rpc/StopSalesMonth.java
@@ -13,67 +13,67 @@ public class StopSalesMonth {
     @ListColumn
     private int month;
 
-    @ListColumn("01")
+    @ListColumn(value = "01", width = 15)
     private Data day_1;
-    @ListColumn("02")
+    @ListColumn(value = "02", width = 15)
     private Data day_2;
-    @ListColumn("03")
+    @ListColumn(value = "03", width = 15)
     private Data day_3;
-    @ListColumn("04")
+    @ListColumn(value = "04", width = 15)
     private Data day_4;
-    @ListColumn("05")
+    @ListColumn(value = "05", width = 15)
     private Data day_5;
-    @ListColumn("06")
+    @ListColumn(value = "06", width = 15)
     private Data day_6;
-    @ListColumn("07")
+    @ListColumn(value = "07", width = 15)
     private Data day_7;
-    @ListColumn("08")
+    @ListColumn(value = "08", width = 15)
     private Data day_8;
-    @ListColumn("09")
+    @ListColumn(value = "09", width = 15)
     private Data day_9;
-    @ListColumn("10")
+    @ListColumn(value = "10", width = 15)
     private Data day_10;
-    @ListColumn("11")
+    @ListColumn(value = "11", width = 15)
     private Data day_11;
-    @ListColumn("12")
+    @ListColumn(value = "12", width = 15)
     private Data day_12;
-    @ListColumn("13")
+    @ListColumn(value = "13", width = 15)
     private Data day_13;
-    @ListColumn("14")
+    @ListColumn(value = "14", width = 15)
     private Data day_14;
-    @ListColumn("15")
+    @ListColumn(value = "15", width = 15)
     private Data day_15;
-    @ListColumn("16")
+    @ListColumn(value = "16", width = 15)
     private Data day_16;
-    @ListColumn("17")
+    @ListColumn(value = "17", width = 15)
     private Data day_17;
-    @ListColumn("18")
+    @ListColumn(value = "18", width = 15)
     private Data day_18;
-    @ListColumn("19")
+    @ListColumn(value = "19", width = 15)
     private Data day_19;
-    @ListColumn("20")
+    @ListColumn(value = "20", width = 15)
     private Data day_20;
-    @ListColumn("21")
+    @ListColumn(value = "21", width = 15)
     private Data day_21;
-    @ListColumn("22")
+    @ListColumn(value = "22", width = 15)
     private Data day_22;
-    @ListColumn("23")
+    @ListColumn(value = "23", width = 15)
     private Data day_23;
-    @ListColumn("24")
+    @ListColumn(value = "24", width = 15)
     private Data day_24;
-    @ListColumn("25")
+    @ListColumn(value = "25", width = 15)
     private Data day_25;
-    @ListColumn("26")
+    @ListColumn(value = "26", width = 15)
     private Data day_26;
-    @ListColumn("27")
+    @ListColumn(value = "27", width = 15)
     private Data day_27;
-    @ListColumn("28")
+    @ListColumn(value = "28", width = 15)
     private Data day_28;
-    @ListColumn("29")
+    @ListColumn(value = "29", width = 15)
     private Data day_29;
-    @ListColumn("30")
+    @ListColumn(value = "30", width = 15)
     private Data day_30;
-    @ListColumn("31")
+    @ListColumn(value = "31", width = 15)
     private Data day_31;
 
 }
