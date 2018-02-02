e4f43f4c12ff427aebaab6e250ac4260590fb03f
miguelperezcolom <miguelperezcolom@gmail.com>
2018-02-02 15:12:46 +0100

    xx

--- mdd-tester/src/main/java/io/mateu/mdd/model/hotelcontract/HotelContract.java
+++ mdd-tester/src/main/java/io/mateu/mdd/model/hotelcontract/HotelContract.java
@@ -3,6 +3,7 @@ package io.mateu.mdd.model.hotelcontract;
 import com.google.common.collect.Lists;
 import io.mateu.erp.model.product.hotel.*;
 import io.mateu.ui.mdd.server.annotations.Action;
+import io.mateu.ui.mdd.server.annotations.FullWidth;
 import io.mateu.ui.mdd.server.annotations.Tab;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,6 +33,7 @@ public class HotelContract {
     @Tab("Terms")
     @Column(name = "terms2")
     @Convert(converter = HotelContractPhotoConverter.class)
+    @FullWidth
     private HotelContractPhoto terms;
 
     @Action(name = "Fill fares")
@@ -47,28 +49,18 @@ public class HotelContract {
             for (int k = 0; k < 5; k++) {
                 fechas.add(new DatesRange(c.getValidFrom().plusDays(j * 10), c.getValidFrom().plusDays((j + 1) * 10 - 1)));
             }
-            Map<String, RoomFare> porHabitacion = new HashMap<>();
+
+            List<LinearFareLine> lineas = new ArrayList<>();
 
             int finalJ = j;
             Lists.newArrayList("DBL", "SUI").stream().forEach((r) -> {
-                RoomFare rf = new RoomFare();
                 Lists.newArrayList("BB", "HB").stream().forEach((b) -> {
-                    BoardFare bf = new BoardFare();
-                    bf.setRoomPrice(new FareValue(false, false, false, 30 + (finalJ * 10)));
-                    bf.setPaxPrice(new FareValue(false, false, false, 60.15 + (finalJ * 10)));
-                    for (int q = 0; q < 2; q++) {
-                        bf.getPaxDiscounts().put(3 + q, new FareValue(false, true, true, q + 10));
-                    }
-                    for (int q = 0; q < 3; q++) {
-                        for (int t = 0; t < 3; t++) {
-                            bf.getChildDiscounts().put(t * 100 + 1 + q, new FareValue(false, true, true, q + 10));
-                        }
-                    }
-                    rf.getFarePerBoard().put(b, bf);
+                    double ad;
+                    lineas.add(new LinearFareLine(r, b, 30 + (finalJ * 10), ad = 60.15 + (finalJ * 10), ad, ad / 2, 0));
                 });
-                porHabitacion.put(r, rf);
             });
-            p.getFares().add(new Fare("Fare " + j, fechas, porHabitacion));
+
+            p.getFares().add(new LinearFare(fechas, "Fare " + j, lineas));
         }
 
         setTerms(p);
