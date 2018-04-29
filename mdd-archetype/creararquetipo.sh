70fa4fe63fded527e07878f34fe67e2f9fe8161a
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2018-04-29 20:25:29 +0200

    first commit

---
+++ mdd-archetype/creararquetipo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mvn clean
+mvn archetype:create-from-project
+cd target/generated-sources/archetype
+mvn install
+cd ../../..
+mvn clean
+
+
+####mvn archetype:generate -DarchetypeCatalog=local
