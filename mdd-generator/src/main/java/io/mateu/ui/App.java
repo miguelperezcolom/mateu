231c21459594d35dfcf4b4b4cfdd8d07034e6a85
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2018-05-22 16:05:40 +0200

    first commit

---
+++ mdd-generator/src/main/java/io/mateu/ui/App.java
@@ -0,0 +1,13 @@
+package io.mateu.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
