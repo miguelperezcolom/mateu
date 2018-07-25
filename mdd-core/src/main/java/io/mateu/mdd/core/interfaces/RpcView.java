f981aea421c8b05b1a0fe20fdcb3d9840023712c
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2018-07-25 15:46:48 +0200

    commit de seguridad

--- mdd-core/src/main/java/io/mateu/mdd/core/interfaces/RpcView.java
+++ mdd-core/src/main/java/io/mateu/mdd/core/interfaces/RpcView.java
@@ -1,5 +1,7 @@
 package io.mateu.mdd.core.interfaces;
 
+import com.vaadin.ui.Grid;
+
 import java.util.List;
 
 public interface RpcView<F, C> {
@@ -7,5 +9,7 @@ public interface RpcView<F, C> {
     List<C> rpc(F filters, int offset, int limit);
 
     int gatherCount(F filters);
+
+    void decorateGrid(Grid<C> grid);
 
 }
