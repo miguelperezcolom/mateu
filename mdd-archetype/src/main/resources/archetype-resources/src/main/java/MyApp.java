6f3d844ff0f221b2933a2c0b6009f75533b791fe
miguelperezcolom <miguelperezcolom@gmail.com>
2018-08-17 19:18:49 +0200

    AA

--- mdd-archetype/src/main/resources/archetype-resources/src/main/java/MyApp.java
+++ mdd-archetype/src/main/resources/archetype-resources/src/main/java/MyApp.java
@@ -21,23 +21,23 @@ public class MyApp extends SimpleMDDApplication {
         return new MDDOpenCRUDAction(Person.class);
     }
 
-    @Action(order = 6)
+    @Action(order = 3)
     public String md5(String s) {
         return Helper.md5(s);
     }
 
-    @Action(order = 7)
+    @Action(order = 4)
     public String getThreadName() {
         return Thread.currentThread().getName();
     }
 
-    @Action(order = 8)
+    @Action(order = 5)
     public String ls() throws IOException {
         return Helper.toHtml(Helper.runCommand("ls -lh"));
     }
 
 
-    @Action(order = 9)
+    @Action(order = 6)
     public String df() throws IOException {
         return Helper.toHtml(Helper.runCommand("df -h"));
     }
