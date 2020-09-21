6099b986ac3cf4e8a29c159950a9af13ed35f6b1
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2020-09-21 10:59:00 +0200

    added image for wiki

--- mdd-showcase/src/main/java/io/mateu/showcase/app/MyUI.java
+++ mdd-showcase/src/main/java/io/mateu/showcase/app/MyUI.java
@@ -5,14 +5,12 @@ import io.mateu.mdd.core.annotations.MateuMDDUI;
 import io.mateu.mdd.core.annotations.Submenu;
 import io.mateu.mdd.core.app.MateuUI;
 import io.mateu.showcase.domain.boundedContexts.educational.model.Grade;
+import io.mateu.showcase.domain.boundedContexts.financial.model.Customer;
 
 @MateuMDDUI(path = "")
 public class MyUI extends MateuUI {
 
-    @Submenu
-    public ConfigMenu config;
-
     @Action
-    public Class grades = Grade.class;
+    public Class customers = Customer.class;
 
 }
---
+++ mdd-showcase/src/main/java/io/mateu/showcase/domain/boundedContexts/financial/model/Customer.java
@@ -0,0 +1,12 @@
+package io.mateu.showcase.domain.boundedContexts.financial.model;
+
+import lombok.MateuMDDEntity;
+
+@MateuMDDEntity
+public class Customer {
+
+    private String name;
+
+    private String address;
+
+}
