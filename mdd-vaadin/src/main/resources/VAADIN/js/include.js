b0249a2f768d9ba57e9cb3ee75096adbae5e9eac
Miguel Pérez <miguel.perez@sembo.se>
2022-12-08 20:50:59 +0100

    fix spinner customization

--- mdd-vaadin/src/main/resources/VAADIN/js/include.js
+++ mdd-vaadin/src/main/resources/VAADIN/js/include.js
@@ -1,4 +1,18 @@
-setTimeout(function () {
-    console.log('Customizando spinner')
-    document.querySelector('.v-loading-indicator').innerHTML = '<div class="spinner"></div>';
-}, 1000)
+document.addEventListener('DOMContentLoaded', () => {
+
+    var f = () => {
+        console.log('Customizando spinner');
+        if (document.querySelector('.v-loading-indicator')) {
+            document.querySelector('.v-loading-indicator').innerHTML = '<div class="spinner"></div>';
+            console.log('spinner customizado')
+        } else {
+            console.log('Reprogramando customización spinner')
+            setTimeout(f, 1000);
+        }
+    }
+
+    // Get all "navbar-burger" elements
+    console.log('Programando customización spinner')
+    setTimeout(f, 1000);
+
+});
--- mdd-vaadin/src/main/webapp/js/include.js
+++ mdd-vaadin/src/main/webapp/js/include.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    var f = () => {
+        console.log('Customizando spinner');
+        if (document.querySelector('.v-loading-indicator')) {
+            document.querySelector('.v-loading-indicator').innerHTML = '<div class="spinner"></div>';
+            console.log('spinner customizado')
+        } else {
+            console.log('Reprogramando customización spinner')
+            setTimeout(f, 1000);
+        }
+    }
+
     // Get all "navbar-burger" elements
     console.log('Programando customización spinner')
-    setTimeout(() => {
-        console.log('Customizando spinner')
-        document.querySelector('.v-loading-indicator').innerHTML = '<div class="spinner"></div>'
-    }, 2000);
+    setTimeout(f, 1000);
 
 });
