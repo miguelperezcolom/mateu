23e6a7c32b5f85abe97fb87ceb75061304323fbc
miguel.perez <miguel.perez@wefox.com>
2022-12-21 22:33:22 +0100

    some format

--- mdd-vaadin/src/main/java/io/mateu/mdd/vaadin/components/fieldBuilders/RpcViewFieldBuilder.java
+++ mdd-vaadin/src/main/java/io/mateu/mdd/vaadin/components/fieldBuilders/RpcViewFieldBuilder.java
@@ -1,58 +1,20 @@
 package io.mateu.mdd.vaadin.components.fieldBuilders;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Sets;
-import com.vaadin.data.*;
-import com.vaadin.data.provider.ListDataProvider;
-import com.vaadin.data.validator.BeanValidator;
-import com.vaadin.icons.VaadinIcons;
-import com.vaadin.server.ClassResource;
-import com.vaadin.server.ExternalResource;
-import com.vaadin.shared.Registration;
-import com.vaadin.shared.ui.ContentMode;
-import com.vaadin.shared.ui.dnd.DropEffect;
-import com.vaadin.shared.ui.dnd.EffectAllowed;
-import com.vaadin.shared.ui.grid.HeightMode;
+import com.vaadin.data.HasValue;
+import com.vaadin.data.Validator;
 import com.vaadin.ui.*;
-import com.vaadin.ui.dnd.DragSourceExtension;
-import com.vaadin.ui.dnd.DropTargetExtension;
-import com.vaadin.ui.themes.ValoTheme;
-import io.mateu.mdd.core.MDD;
 import io.mateu.mdd.core.app.AbstractAction;
-import io.mateu.mdd.core.dataProviders.JPQLListDataProvider;
 import io.mateu.mdd.core.interfaces.AbstractStylist;
-import io.mateu.mdd.core.interfaces.Card;
-import io.mateu.mdd.core.interfaces.GridDecorator;
-import io.mateu.mdd.core.ui.MDDUIAccessor;
-import io.mateu.mdd.shared.CSS;
-import io.mateu.mdd.shared.annotations.*;
-import io.mateu.mdd.shared.data.FareValue;
-import io.mateu.mdd.shared.interfaces.IResource;
+import io.mateu.mdd.shared.annotations.FieldGroup;
 import io.mateu.mdd.shared.interfaces.RpcView;
 import io.mateu.mdd.shared.reflection.FieldInterfaced;
-import io.mateu.mdd.vaadin.MateuUI;
-import io.mateu.mdd.vaadin.components.views.ListViewComponent;
 import io.mateu.mdd.vaadin.components.views.RpcListViewComponent;
 import io.mateu.mdd.vaadin.data.MDDBinder;
-import io.mateu.mdd.vaadin.util.VaadinHelper;
-import io.mateu.reflection.*;
-import io.mateu.util.Helper;
-import io.mateu.util.notification.Notifier;
+import io.mateu.reflection.ReflectionHelper;
 
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.lang.reflect.*;
-import java.text.DecimalFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
 
 public class RpcViewFieldBuilder extends AbstractFieldBuilder {
 
@@ -63,7 +25,11 @@ public class RpcViewFieldBuilder extends AbstractFieldBuilder {
     }
 
     @Override
-    public Component build(VerticalLayout fieldGroup, HorizontalLayout fieldGroupHeader, FieldInterfaced field, Object object, Layout container, MDDBinder binder, Map<HasValue, List<Validator>> validators, AbstractStylist stylist, Map<FieldInterfaced, Component> allFieldContainers, boolean forSearchFilter, Map<String, List<AbstractAction>> attachedActions) {
+    public Component build(VerticalLayout fieldGroup, HorizontalLayout fieldGroupHeader, FieldInterfaced field,
+                           Object object, Layout container, MDDBinder binder,
+                           Map<HasValue, List<Validator>> validators, AbstractStylist stylist,
+                           Map<FieldInterfaced, Component> allFieldContainers, boolean forSearchFilter,
+                           Map<String, List<AbstractAction>> attachedActions) {
         Component r = new Label("Failed to build the list");
 
         try {
