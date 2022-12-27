a0e9a7883214c8418a6ee35788fe0ff422b3da95
Miguel Pérez <miguel.perez@sembo.se>
2022-12-27 15:01:36 +0100

    restore basics addon

---
+++ mdd-addon/src/main/java/com/vaadin/client/MouseOverVerticalLayoutRpc.java
@@ -0,0 +1,9 @@
+package com.vaadin.client;
+
+import com.vaadin.shared.communication.ServerRpc;
+
+public interface MouseOverVerticalLayoutRpc extends ServerRpc {
+
+    void mousedOver();
+
+}
---
+++ mdd-addon/src/main/java/com/vaadin/client/VMouseOverVerticalLayout.java
@@ -0,0 +1,7 @@
+package com.vaadin.client;
+
+import com.vaadin.client.ui.VVerticalLayout;
+
+public class VMouseOverVerticalLayout extends VVerticalLayout {
+
+}
