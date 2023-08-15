2af0bea386b19773a49e0a7cfb259849c7749e1e
miguel.perez <miguel.perez@wefox.com>
2023-08-15 12:47:18 +0200

    refactor

--- mdd-remote/src/main/java/io/mateu/remote/application/UploadService.java
+++ mdd-remote/src/main/java/io/mateu/remote/application/UploadService.java
@@ -1,26 +1,42 @@
 package io.mateu.remote.application;
 
+import io.mateu.remote.domain.files.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import javax.naming.AuthenticationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-public interface UploadService {
+@Service
+public class UploadService {
 
-    ResponseEntity<Resource> serveFile(String fileId,
+    @Autowired
+    StorageService storageService;
+
+    public ResponseEntity<Resource> serveFile(String fileId,
                                               String filename)
-            throws AuthenticationException;
+            throws AuthenticationException {
 
-    String getFileUrl(String fileId) throws AuthenticationException;
+        Resource file = storageService.loadAsResource(fileId, filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
 
-    Mono<Void> handleFileUpload(String fileId,
+    public String getFileUrl(String fileId) throws AuthenticationException {
+        return storageService.getUrl(fileId);
+    }
+
+    public Mono<Void> handleFileUpload(String fileId,
                                        Mono<FilePart> file)
-            throws AuthenticationException, ExecutionException, InterruptedException, TimeoutException;
+            throws AuthenticationException, ExecutionException, InterruptedException, TimeoutException {
+        return storageService.store(fileId, file);
+    }
 
 
 }
