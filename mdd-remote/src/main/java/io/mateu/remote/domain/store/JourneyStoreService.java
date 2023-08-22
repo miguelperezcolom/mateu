5596faf06ce5795a5ce51b25b1eb63725cd71750
miguel.perez <miguel.perez@wefox.com>
2023-08-22 20:00:03 +0200

    fix new for nested crud

--- mdd-remote/src/main/java/io/mateu/remote/domain/store/JourneyStoreService.java
+++ mdd-remote/src/main/java/io/mateu/remote/domain/store/JourneyStoreService.java
@@ -111,6 +111,15 @@ public class JourneyStoreService {
                 if (listField != null) {
                     return jpaRpcCrudFactory.create(actualInstance, listField);
                 }
+            } else if (actualInstance instanceof ObjectEditor) {
+                ObjectEditor objectEditor = (ObjectEditor) actualInstance;
+                Object pojo = ReflectionHelper.newInstance(objectEditor.getType());
+                Object filled = Helper.fromJson(Helper.toJson(objectEditor.getData()), objectEditor.getType());
+                ReflectionHelper.copy(filled, pojo);
+                FieldInterfaced listField = ReflectionHelper.getFieldByName(pojo.getClass(), listId);
+                if (listField != null) {
+                    return (Listing) ReflectionHelper.getValue(listField, pojo);
+                }
             } else {
                  return (Listing) ReflectionHelper.getValue(listId, actualInstance);
             }
