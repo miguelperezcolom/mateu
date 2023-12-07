328b8e50599db04d80d98b3e81a6762189dba322
miguel.perez <miguel.perez@wefox.com>
2023-12-07 17:23:04 +0100

    improve pipelines

--- .github/scripts/19-deploy-to-hetzner.sh
+++ .github/scripts/19-deploy-to-hetzner.sh
@@ -2,7 +2,6 @@
 
 mvn -ntp -B package --file demo/pom.xml
 echo $HETZNER_SSH_PUBLIC_KEY > id_rsa
-echo "\n" >> id_rsa
 chmod 0600 id_rsa
 ls -l
 scp -o "StrictHostKeyChecking=no" -i id_rsa demo/target/demo-0.0.1-SNAPSHOT.jar root@explorer.mateu.io:/root/mateu
