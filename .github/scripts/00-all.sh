c5eaed23db7d026d7b9c12f9d2d1447012dfedd8
miguel.perez <miguel.perez@wefox.com>
2023-12-07 14:01:04 +0100

    improve pipelines

--- .github/scripts/00-all.sh
+++ .github/scripts/00-all.sh
@@ -4,5 +4,6 @@ echo $RELEASE_VERSION
 
 ./.github/scripts/10-build-frontend.sh
 ./.github/scripts/11-build-backend.sh
+./.github/scripts/20-publish-to-maven-central.sh
 
 
---
+++ .github/scripts/20-publish-to-maven-central.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mvn deploy -f backend/pom.xml
+
+
+
