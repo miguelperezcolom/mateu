33c23ef0e3d150e2410bfb1573e6b6ff21e010cf
miguel.perez <miguel.perez@wefox.com>
2023-12-07 13:47:49 +0100

    improve pipelines

--- .github/scripts/00-all.sh
+++ .github/scripts/00-all.sh
@@ -3,5 +3,6 @@
 echo $RELEASE_VERSION
 
 ./.github/scripts/10-build-frontend.sh
+./.github/scripts/11-build-backend.sh
 
 
---
+++ .github/scripts/11-build-backend.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat ~/.m2/settings.xml
+cp -f settings.xml ~/.m2
+
+grep -rl '<version>0.0.1-MATEU</version>' --exclude-dir=.git backend | grep pom.xml | xargs sed -i 's/0\.0\.1\-MATEU/$RELEASE_VERSION/g'
+
+mvn -B install --file backend/pom.xml
+
+
+
