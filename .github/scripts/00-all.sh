c2e14d8036f8b05201e15571fbb28a1c975d490b
miguel.perez <miguel.perez@wefox.com>
2023-12-09 13:03:30 +0100

    improve pipelines

--- .github/scripts/00-all.sh
+++ .github/scripts/00-all.sh
@@ -7,6 +7,6 @@ echo $RELEASE_VERSION
 ./.github/scripts/11-build-backend.sh
 ./.github/scripts/12-build-demo.sh
 # ./.github/scripts/19-deploy-to-hetzner.sh
-#./.github/scripts/20-publish-to-maven-central.sh
+./.github/scripts/20-publish-to-maven-central.sh
 
 
