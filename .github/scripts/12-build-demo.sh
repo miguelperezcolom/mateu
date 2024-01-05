ad1d890ac2b51f009cab3a284f48550508df8ce3
miguel.perez <miguel.perez@wefox.com>
2024-01-05 11:12:37 +0100

    fix back action

--- .github/scripts/12-build-demo.sh
+++ .github/scripts/12-build-demo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+grep -rl '<version>0.0.1-MATEU</version>' --exclude-dir=.git demo | grep pom.xml | xargs sed -i "s/0\.0\.1\-MATEU/$RELEASE_VERSION/g"
 cat demo/pom.xml
 mvn -ntp -B package --file demo/pom.xml
 cp demo/target/demo-0.0.1-SNAPSHOT.jar .
