c82aff94bdfe910aa164a00d1bb57cfd79c2f9e3
Miguel Pérez <miguel.perez@sembo.se>
2024-03-12 16:31:23 +0100

    fix missing beans conditionals

--- backend/core/src/main/java/io/mateu/mdd/springboot/FakeEntitySerializer.java
+++ backend/core/src/main/java/io/mateu/mdd/springboot/FakeEntitySerializer.java
@@ -2,12 +2,13 @@ package io.mateu.mdd.springboot;
 
 import io.mateu.util.persistence.EntitySerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
-@ConditionalOnMissingBean(type = "EntitySerializer")
+@ConditionalOnMissingClass("io.mateu.mdd.json.MateuEntitySerializer")
 public class FakeEntitySerializer implements EntitySerializer {
     @Override
     public Map<String, Object> toMap(Object entity) throws Exception {
--- backend/core/src/main/java/io/mateu/mdd/springboot/FakeMDDOpenCRUDActionViewBuilder.java
+++ backend/core/src/main/java/io/mateu/mdd/springboot/FakeMDDOpenCRUDActionViewBuilder.java
@@ -4,10 +4,11 @@ import io.mateu.mdd.core.app.MDDOpenCRUDAction;
 import io.mateu.mdd.core.app.MDDOpenCRUDActionViewBuilder;
 import io.mateu.mdd.core.interfaces.Crud;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.stereotype.Service;
 
 @Service
-@ConditionalOnMissingBean(type = "MDDOpenCRUDActionViewBuilder")
+@ConditionalOnMissingClass("io.mateu.mdd.ui.cruds.JpaMDDOpenCrudActionViewBuilder")
 public class FakeMDDOpenCRUDActionViewBuilder implements MDDOpenCRUDActionViewBuilder {
     @Override
     public Crud buildView(MDDOpenCRUDAction action) throws Exception {
