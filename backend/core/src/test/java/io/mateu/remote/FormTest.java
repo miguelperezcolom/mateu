8804b466625d16564c94b708f4dbb5d8b12aee7b
miguel.perez <miguel.perez@wefox.com>
2024-03-31 12:54:32 +0200

    extend expected json

--- backend/core/src/test/java/io/mateu/remote/FormTest.java
+++ backend/core/src/test/java/io/mateu/remote/FormTest.java
@@ -51,12 +51,425 @@ public class FormTest {
     JourneyCreationRq creationRq = JourneyCreationRq.builder().build();
     String expectedJson = """
             {
-            
-            "journey" : {
+              "journey" : {
                 "type" : "io.mateu.remote.sampleui.MyUi",
-                "status" : "Pending"
+                "status" : "Pending",
+                "statusMessage" : "Please fill the form",
+                "currentStepId" : "form",
+                "currentStepDefinitionId" : "io.mateu.remote.sampleui.MyUi"
+              },
+              "step" : {
+                "id" : "form",
+                "name" : "This is a sample form",
+                "type" : "io.mateu.remote.sampleui.MyUi",
+                "view" : {
+                  "title" : null,
+                  "subtitle" : null,
+                  "messages" : [ ],
+                  "header" : {
+                    "classes" : null,
+                    "components" : [ ]
+                  },
+                  "left" : {
+                    "classes" : null,
+                    "components" : [ ]
+                  },
+                  "main" : {
+                    "classes" : null,
+                    "components" : [ {
+                      "metadata" : {
+                        "type" : "Form",
+                        "type" : "Form",
+                        "dataPrefix" : "",
+                        "icon" : null,
+                        "title" : "This is a sample form",
+                        "readOnly" : false,
+                        "subtitle" : null,
+                        "status" : null,
+                        "badges" : [ ],
+                        "tabs" : [ ],
+                        "banners" : [ ],
+                        "sections" : [ {
+                          "id" : "section_0",
+                          "tabId" : "",
+                          "caption" : "",
+                          "description" : "",
+                          "readOnly" : false,
+                          "type" : "Card",
+                          "leftSideImageUrl" : "",
+                          "topImageUrl" : "",
+                          "actions" : [ ],
+                          "fieldGroups" : [ {
+                            "id" : "fieldgroup_1_0",
+                            "caption" : "",
+                            "lines" : [ {
+                              "fields" : [ {
+                                "id" : "name",
+                                "type" : "string",
+                                "stereotype" : "input",
+                                "observed" : false,
+                                "caption" : "Name",
+                                "placeholder" : null,
+                                "cssClasses" : null,
+                                "description" : null,
+                                "badges" : [ ],
+                                "validations" : [ ],
+                                "attributes" : [ ]
+                              } ]
+                            }, {
+                              "fields" : [ {
+                                "id" : "age",
+                                "type" : "int",
+                                "stereotype" : "input",
+                                "observed" : false,
+                                "caption" : "Age",
+                                "placeholder" : null,
+                                "cssClasses" : null,
+                                "description" : null,
+                                "badges" : [ ],
+                                "validations" : [ ],
+                                "attributes" : [ ]
+                              } ]
+                            }, {
+                              "fields" : [ {
+                                "id" : "assessment",
+                                "type" : "string",
+                                "stereotype" : "input",
+                                "observed" : false,
+                                "caption" : "Assessment",
+                                "placeholder" : null,
+                                "cssClasses" : null,
+                                "description" : null,
+                                "badges" : [ ],
+                                "validations" : [ ],
+                                "attributes" : [ ]
+                              } ]
+                            } ]
+                          } ]
+                        } ],
+                        "actions" : [ {
+                          "id" : "component-0___assess",
+                          "caption" : "Assess",
+                          "type" : "Primary",
+                          "visible" : true,
+                          "validationRequired" : true,
+                          "confirmationRequired" : false,
+                          "rowsSelectedRequired" : false,
+                          "confirmationTexts" : {
+                            "title" : "Please confirm",
+                            "message" : "",
+                            "action" : "Assess"
+                          },
+                          "target" : "SameLane",
+                          "modalStyle" : "",
+                          "customEvent" : "",
+                          "href" : ""
+                        } ],
+                        "mainActions" : [ ],
+                        "validations" : null
+                      },
+                      "id" : "component-0",
+                      "attributes" : { }
+                    } ]
+                  },
+                  "right" : {
+                    "classes" : null,
+                    "components" : [ ]
+                  },
+                  "footer" : {
+                    "classes" : null,
+                    "components" : [ ]
+                  }
+                },
+                "data" : {
+                  "name" : "Mateu",
+                  "assessment" : null,
+                  "age" : 0
+                },
+                "rules" : [ ],
+                "previousStepId" : null
+              },
+              "store" : {
+                "journeyId" : "yy",
+                "journeyTypeId" : "io.mateu.remote.sampleui.MyUi",
+                "remoteBaseUrl" : null,
+                "remoteJourneyTypeId" : null,
+                "journeyClass" : null,
+                "journeyData" : null,
+                "journey" : {
+                  "type" : "io.mateu.remote.sampleui.MyUi",
+                  "status" : "Pending",
+                  "statusMessage" : "Please fill the form",
+                  "currentStepId" : "form",
+                  "currentStepDefinitionId" : "io.mateu.remote.sampleui.MyUi"
+                },
+                "steps" : {
+                  "form" : {
+                    "id" : "form",
+                    "name" : "This is a sample form",
+                    "type" : "io.mateu.remote.sampleui.MyUi",
+                    "view" : {
+                      "title" : null,
+                      "subtitle" : null,
+                      "messages" : [ ],
+                      "header" : {
+                        "classes" : null,
+                        "components" : [ ]
+                      },
+                      "left" : {
+                        "classes" : null,
+                        "components" : [ ]
+                      },
+                      "main" : {
+                        "classes" : null,
+                        "components" : [ {
+                          "metadata" : {
+                            "type" : "Form",
+                            "dataPrefix" : "",
+                            "icon" : null,
+                            "title" : "This is a sample form",
+                            "readOnly" : false,
+                            "subtitle" : null,
+                            "status" : null,
+                            "badges" : [ ],
+                            "tabs" : [ ],
+                            "banners" : [ ],
+                            "sections" : [ {
+                              "id" : "section_0",
+                              "tabId" : "",
+                              "caption" : "",
+                              "description" : "",
+                              "readOnly" : false,
+                              "type" : "Card",
+                              "leftSideImageUrl" : "",
+                              "topImageUrl" : "",
+                              "actions" : [ ],
+                              "fieldGroups" : [ {
+                                "id" : "fieldgroup_1_0",
+                                "caption" : "",
+                                "lines" : [ {
+                                  "fields" : [ {
+                                    "id" : "name",
+                                    "type" : "string",
+                                    "stereotype" : "input",
+                                    "observed" : false,
+                                    "caption" : "Name",
+                                    "placeholder" : null,
+                                    "cssClasses" : null,
+                                    "description" : null,
+                                    "badges" : [ ],
+                                    "validations" : [ ],
+                                    "attributes" : [ ]
+                                  } ]
+                                }, {
+                                  "fields" : [ {
+                                    "id" : "age",
+                                    "type" : "int",
+                                    "stereotype" : "input",
+                                    "observed" : false,
+                                    "caption" : "Age",
+                                    "placeholder" : null,
+                                    "cssClasses" : null,
+                                    "description" : null,
+                                    "badges" : [ ],
+                                    "validations" : [ ],
+                                    "attributes" : [ ]
+                                  } ]
+                                }, {
+                                  "fields" : [ {
+                                    "id" : "assessment",
+                                    "type" : "string",
+                                    "stereotype" : "input",
+                                    "observed" : false,
+                                    "caption" : "Assessment",
+                                    "placeholder" : null,
+                                    "cssClasses" : null,
+                                    "description" : null,
+                                    "badges" : [ ],
+                                    "validations" : [ ],
+                                    "attributes" : [ ]
+                                  } ]
+                                } ]
+                              } ]
+                            } ],
+                            "actions" : [ {
+                              "id" : "component-0___assess",
+                              "caption" : "Assess",
+                              "type" : "Primary",
+                              "visible" : true,
+                              "validationRequired" : true,
+                              "confirmationRequired" : false,
+                              "rowsSelectedRequired" : false,
+                              "confirmationTexts" : {
+                                "title" : "Please confirm",
+                                "message" : "",
+                                "action" : "Assess"
+                              },
+                              "target" : "SameLane",
+                              "modalStyle" : "",
+                              "customEvent" : "",
+                              "href" : ""
+                            } ],
+                            "mainActions" : [ ],
+                            "validations" : null
+                          },
+                          "id" : "component-0",
+                          "attributes" : { }
+                        } ]
+                      },
+                      "right" : {
+                        "classes" : null,
+                        "components" : [ ]
+                      },
+                      "footer" : {
+                        "classes" : null,
+                        "components" : [ ]
+                      }
+                    },
+                    "data" : {
+                      "name" : "Mateu",
+                      "assessment" : null,
+                      "age" : 0
+                    },
+                    "rules" : [ ],
+                    "previousStepId" : null
+                  }
+                },
+                "initialStep" : {
+                  "id" : "form",
+                  "name" : "This is a sample form",
+                  "type" : "io.mateu.remote.sampleui.MyUi",
+                  "view" : {
+                    "title" : null,
+                    "subtitle" : null,
+                    "messages" : [ ],
+                    "header" : {
+                      "classes" : null,
+                      "components" : [ ]
+                    },
+                    "left" : {
+                      "classes" : null,
+                      "components" : [ ]
+                    },
+                    "main" : {
+                      "classes" : null,
+                      "components" : [ {
+                        "metadata" : {
+                          "type" : "Form",
+                          "dataPrefix" : "",
+                          "icon" : null,
+                          "title" : "This is a sample form",
+                          "readOnly" : false,
+                          "subtitle" : null,
+                          "status" : null,
+                          "badges" : [ ],
+                          "tabs" : [ ],
+                          "banners" : [ ],
+                          "sections" : [ {
+                            "id" : "section_0",
+                            "tabId" : "",
+                            "caption" : "",
+                            "description" : "",
+                            "readOnly" : false,
+                            "type" : "Card",
+                            "leftSideImageUrl" : "",
+                            "topImageUrl" : "",
+                            "actions" : [ ],
+                            "fieldGroups" : [ {
+                              "id" : "fieldgroup_1_0",
+                              "caption" : "",
+                              "lines" : [ {
+                                "fields" : [ {
+                                  "id" : "name",
+                                  "type" : "string",
+                                  "stereotype" : "input",
+                                  "observed" : false,
+                                  "caption" : "Name",
+                                  "placeholder" : null,
+                                  "cssClasses" : null,
+                                  "description" : null,
+                                  "badges" : [ ],
+                                  "validations" : [ ],
+                                  "attributes" : [ ]
+                                } ]
+                              }, {
+                                "fields" : [ {
+                                  "id" : "age",
+                                  "type" : "int",
+                                  "stereotype" : "input",
+                                  "observed" : false,
+                                  "caption" : "Age",
+                                  "placeholder" : null,
+                                  "cssClasses" : null,
+                                  "description" : null,
+                                  "badges" : [ ],
+                                  "validations" : [ ],
+                                  "attributes" : [ ]
+                                } ]
+                              }, {
+                                "fields" : [ {
+                                  "id" : "assessment",
+                                  "type" : "string",
+                                  "stereotype" : "input",
+                                  "observed" : false,
+                                  "caption" : "Assessment",
+                                  "placeholder" : null,
+                                  "cssClasses" : null,
+                                  "description" : null,
+                                  "badges" : [ ],
+                                  "validations" : [ ],
+                                  "attributes" : [ ]
+                                } ]
+                              } ]
+                            } ]
+                          } ],
+                          "actions" : [ {
+                            "id" : "component-0___assess",
+                            "caption" : "Assess",
+                            "type" : "Primary",
+                            "visible" : true,
+                            "validationRequired" : true,
+                            "confirmationRequired" : false,
+                            "rowsSelectedRequired" : false,
+                            "confirmationTexts" : {
+                              "title" : "Please confirm",
+                              "message" : "",
+                              "action" : "Assess"
+                            },
+                            "target" : "SameLane",
+                            "modalStyle" : "",
+                            "customEvent" : "",
+                            "href" : ""
+                          } ],
+                          "mainActions" : [ ],
+                          "validations" : null
+                        },
+                        "id" : "component-0",
+                        "attributes" : { }
+                      } ]
+                    },
+                    "right" : {
+                      "classes" : null,
+                      "components" : [ ]
+                    },
+                    "footer" : {
+                      "classes" : null,
+                      "components" : [ ]
+                    }
+                  },
+                  "data" : {
+                    "name" : "Mateu",
+                    "assessment" : null,
+                    "age" : 0
+                  },
+                  "rules" : [ ],
+                  "previousStepId" : null
+                },
+                "created" : null,
+//                "lastAccess" : "2024-03-31T12:50:58.678268",
+                "lastUsedFilters" : { },
+                "lastUsedSorting" : { }
               }
-            
             }
             """;
 
