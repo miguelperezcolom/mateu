07bcadc90e2b9fb29b101a99d7e0407a3e652881
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2024-06-15 11:39:37 +0200

    Remove deprecated journeyrepo jpa (#128)
    * change trigger for build and publish pipeline
    
    * fix release version
    
    * remove deprecated journeyrepo-jpa module
    
    ---------
    
    Co-authored-by: Miguel Pérez <miguel.perez@sembo.se>

--- backend/optionals/journeyrepo-jpa/src/main/java/io/mateu/remote/infra/jpa/MateuRemoteConfig.java
+++
@@ -1,8 +0,0 @@
-package io.mateu.remote.infra.jpa;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync
-public class MateuRemoteConfig {}
