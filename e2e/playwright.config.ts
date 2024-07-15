02f40fbce80abf399b74d61e5a03ec88224e97de
miguelperezcolom <miguelperezcolom@gmail.com>
2024-07-15 17:24:02 +0200

    e2e tests on chromium only

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -46,7 +46,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
