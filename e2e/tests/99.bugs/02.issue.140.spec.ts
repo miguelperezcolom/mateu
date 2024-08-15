2a9c747f677d28a669238c8e1ce722dcd6a42347
miguelperezcolom <miguelperezcolom@gmail.com>
2024-08-15 11:27:15 +0200

    add test

---
+++ e2e/tests/99.bugs/02.issue.140.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('navigation in modals works as expected', async ({ page }) => {
+  await page.goto('/')
+
+  // Expect a title "to contain" a substring.
+  await page.getByRole('menuitem', { name: 'Forms' }).click()
+  await page.getByRole('option', { name: 'Modals'}).click()
+  await expect(page.getByRole('heading', { name: 'This is the title' })).toBeVisible()
+
+  await page.getByLabel('Other save options').locator('svg').hover()
+  await page.getByRole('option', { name: 'Open modal', exact: true }).locator('div').click()
+  await expect(page.getByText('Wizard page 1')).toBeVisible()
+  await expect(page.getByText('Back')).toBeVisible({visible: false})
+  await page.getByRole('button', { name: 'Go to next page' }).click()
+  await expect(page.getByText('Wizard page 2')).toBeVisible()
+  await page.getByRole('button').first().click()
+
+  await page.getByLabel('Other save options').locator('svg').hover()
+  await page.getByRole('option', { name: 'Assess', exact: true }).locator('div').click()
+  await expect(page.getByText('Back')).toBeVisible({visible: false})
+
+  await page.getByLabel('Other save options').locator('svg').hover()
+  await page.getByRole('option', { name: 'Open modal', exact: true }).locator('div').click()
+  await expect(page.getByText('Wizard page 1')).toBeVisible()
+  await expect(page.getByText('Back')).toBeVisible({visible: false})
+
+
+});
