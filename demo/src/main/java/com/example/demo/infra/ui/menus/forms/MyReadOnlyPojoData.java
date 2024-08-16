c57b358c6c1fdb55bb71fc082b1450253ba113df
miguelperezcolom <miguelperezcolom@gmail.com>
2024-08-16 11:26:52 +0200

    add test

---
+++ demo/src/main/java/com/example/demo/infra/ui/menus/forms/MyReadOnlyPojoData.java
@@ -0,0 +1,14 @@
+package com.example.demo.infra.ui.menus.forms;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter@Setter
+public class MyReadOnlyPojoData {
+
+    String name = "Mateu";
+    String id;
+
+}
