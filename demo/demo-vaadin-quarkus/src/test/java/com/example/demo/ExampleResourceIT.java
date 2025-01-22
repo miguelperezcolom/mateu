8d162248d19c7576db17fcff33e180b54b0a76fe
miguelperezcolom <miguelperezcolom@gmail.com>
2025-01-22 20:21:43 +0100

    add quarkus module

---
+++ demo/demo-vaadin-quarkus/src/test/java/com/example/demo/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
