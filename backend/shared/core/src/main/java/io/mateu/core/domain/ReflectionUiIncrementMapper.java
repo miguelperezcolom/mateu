7440d3dcc884a3c5a87027413ecca32a9cd065d4
Miguel Perez Colom <miguel.perez.colom@oracle.com>
2025-05-07 07:45:45 +0200

    Add MapsToDto interface.

--- backend/shared/core/src/main/java/io/mateu/core/domain/ReflectionUiIncrementMapper.java
+++ backend/shared/core/src/main/java/io/mateu/core/domain/ReflectionUiIncrementMapper.java
@@ -2,6 +2,7 @@ package io.mateu.core.domain;
 
 import io.mateu.dtos.UIIncrementDto;
 import io.mateu.uidl.interfaces.HttpRequest;
+import io.mateu.uidl.interfaces.MapsToDto;
 import jakarta.inject.Named;
 import reactor.core.publisher.Mono;
 
@@ -14,6 +15,9 @@ public class ReflectionUiIncrementMapper implements UiIncrementMapper {
     }
     if (instance instanceof UIIncrementDto uiIncrementDto) {
       return Mono.just(uiIncrementDto);
+    }
+    if (instance instanceof MapsToDto mapsToDto) {
+      return Mono.just(mapsToDto.toUIIncrementDto());
     }
     return Mono.just(new UIIncrementDto(null, null, null, null));
   }
---
+++ backend/shared/uidl/src/main/java/io/mateu/uidl/interfaces/MapsToDto.java
@@ -0,0 +1,8 @@
+package io.mateu.uidl.interfaces;
+
+import io.mateu.dtos.UIIncrementDto;
+
+public interface MapsToDto {
+
+    UIIncrementDto toUIIncrementDto();
+}
