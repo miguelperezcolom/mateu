34322dcf57b9d0db69e6d90e3a00d51b3e7a07f5
Miguel Perez Colom <miguel.perez.colom@oracle.com>
2025-05-10 09:52:33 +0200

    Fix compilation.

--- demo/demo-vaadin-quarkus/src/main/java/com/example/demo/ExampleResource.java
+++ demo/demo-vaadin-quarkus/src/main/java/com/example/demo/ExampleResource.java
@@ -5,14 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import java.net.http.HttpRequest;
-
 @Path("/hello")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello(HttpRequest) {
+    public String hello() {
         return "Hello from Quarkus REST";
     }
 }
