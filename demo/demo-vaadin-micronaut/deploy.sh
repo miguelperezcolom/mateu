85526fc54f40e67df9347d60f4c847e898b54f8f
miguelperezcolom <miguelperezcolom@gmail.com>
2025-09-09 03:45:24 +0200

    Add deployment stuff for micronaut based demo.

---
+++ demo/demo-vaadin-micronaut/deploy.sh
@@ -0,0 +1,10 @@
+docker login
+docker buildx build --platform=linux/x86 -t miguelperezcolom/mateu-demo .
+#docker build -t miguelperezcolom/mateu-demo .
+export VERSION=3.0-alpha.65
+docker image tag miguelperezcolom/mateu-demo miguelperezcolom/mateu-demo:latest
+docker image tag miguelperezcolom/mateu-demo miguelperezcolom/mateu-demo:$VERSION
+echo "pushing latest"
+docker push miguelperezcolom/mateu-demo:latest
+echo "pushing $VERSION"
+docker push miguelperezcolom/mateu-demo:$VERSION
