77dc34cee47aed15855ca06a388b59522d1cd119
miguelperezcolom <miguelperezcolom@gmail.com>
2025-11-16 01:38:54 +0100

    Fix nested app routing.

--- backend/shared/core/src/main/java/io/mateu/core/application/runaction/RunActionUseCase.java
+++ backend/shared/core/src/main/java/io/mateu/core/application/runaction/RunActionUseCase.java
@@ -360,20 +360,20 @@ public class RunActionUseCase {
       String route, String consumedRoute, HttpRequest httpRequest) {
     var cleanRoute = "/_page".equals(route) ? "" : route;
     for (RouteResolver resolver :
-            beanProvider.getBeans(RouteResolver.class).stream()
-                    .sorted(Comparator.comparingInt(a -> a.weight(cleanRoute)))
-                    .filter(routeResolver -> routeResolver.supportsRoute(cleanRoute))
-                    .filter(
-                            resolver -> {
-                              var resolved = resolver.resolveRoute(cleanRoute, httpRequest);
-                              return AppSupplier.class.isAssignableFrom(resolved)
-                                      || App.class.isAssignableFrom(resolved)
-                                      || io.mateu.uidl.interfaces.App.class.isAssignableFrom(resolved)
-                                      || resolved.isAnnotationPresent(MateuUI.class);
-                            })
-                    .toList()) {
+        beanProvider.getBeans(RouteResolver.class).stream()
+            .sorted(Comparator.comparingInt(a -> a.weight(cleanRoute)))
+            .filter(routeResolver -> routeResolver.supportsRoute(cleanRoute))
+            .filter(
+                resolver -> {
+                  var resolved = resolver.resolveRoute(cleanRoute, httpRequest);
+                  return AppSupplier.class.isAssignableFrom(resolved)
+                      || App.class.isAssignableFrom(resolved)
+                      || io.mateu.uidl.interfaces.App.class.isAssignableFrom(resolved)
+                      || resolved.isAnnotationPresent(MateuUI.class);
+                })
+            .toList()) {
       if (resolver.supportsRoute(route)
-              && ("".equals(consumedRoute) || !resolver.supportsRoute(consumedRoute))) {
+          && ("".equals(consumedRoute) || !resolver.supportsRoute(consumedRoute))) {
         return resolver.resolveRoute(route, httpRequest).getName();
       }
     }
