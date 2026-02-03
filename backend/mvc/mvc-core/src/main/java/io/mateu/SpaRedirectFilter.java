e06dea35eb68267e2b9259ad95c60d1e1159df3d
miguelperezcolom <miguelperezcolom@gmail.com>
2026-02-03 22:59:02 +0100

    Fix filter.

--- backend/mvc/mvc-core/src/main/java/io/mateu/SpaRedirectFilter.java
+++ backend/mvc/mvc-core/src/main/java/io/mateu/SpaRedirectFilter.java
@@ -29,48 +29,52 @@ public class SpaRedirectFilter extends OncePerRequestFilter {
 
     String path = request.getRequestURI();
 
-    // 1. Si es la raíz o tiene punto (recurso), dejar pasar
-    if (path.equals("/") || path.contains(".")) {
-      filterChain.doFilter(request, response);
-      return;
-    }
+    if (!path.startsWith("/actuator")) {
 
-    // 2. Comprobar si existe un controlador mapeado para esta ruta
-    boolean hasHandler = false;
-    try {
-      for (HandlerMapping hm : handlerMappings) {
-        var handler = hm.getHandler(request);
-        if (handler != null
-            && handler.getHandler() != null
-            && !(handler.getHandler() instanceof ResourceHttpRequestHandler)) {
-          hasHandler = true;
-          break;
+        // 1. Si es la raíz o tiene punto (recurso), dejar pasar
+        if (path.equals("/") || path.contains(".")) {
+            filterChain.doFilter(request, response);
+            return;
         }
-      }
-    } catch (Exception e) {
-      hasHandler = false;
-    }
 
-    // 3. Si NO hay un controlador, es una ruta de la UI -> Forward al index
-    if (!hasHandler) {
-      boolean found = false;
-      for (MateuController controller : MateuBeanProvider.getBeans(MateuController.class)) {
-        if (!"".equals(controller.getBaseUrl())) {
-          if (path.startsWith(controller.getBaseUrl())) {
-            path = controller.getBaseUrl();
-            found = true;
-            break;
-          }
+        // 2. Comprobar si existe un controlador mapeado para esta ruta
+        boolean hasHandler = false;
+        try {
+            for (HandlerMapping hm : handlerMappings) {
+                var handler = hm.getHandler(request);
+                if (handler != null
+                        && handler.getHandler() != null
+                        && !(handler.getHandler() instanceof ResourceHttpRequestHandler)) {
+                    hasHandler = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            hasHandler = false;
         }
-      }
-      if (!found) {
-        if (path.startsWith("/mateu")) {
-        } else {
-          path = "/";
+
+        // 3. Si NO hay un controlador, es una ruta de la UI -> Forward al index
+        if (!hasHandler) {
+            boolean found = false;
+            for (MateuController controller : MateuBeanProvider.getBeans(MateuController.class)) {
+                if (!"".equals(controller.getBaseUrl())) {
+                    if (path.startsWith(controller.getBaseUrl())) {
+                        path = controller.getBaseUrl();
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            if (!found) {
+                if (path.startsWith("/mateu")) {
+                } else {
+                    path = "/";
+                }
+            }
+            request.getRequestDispatcher(path).forward(request, response);
+            return;
         }
-      }
-      request.getRequestDispatcher(path).forward(request, response);
-      return;
+
     }
 
     filterChain.doFilter(request, response);
