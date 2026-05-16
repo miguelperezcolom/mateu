1aca1bb0d2e595f39cb305bd3c68af7ee41be6ab
miguelperezcolom <miguelperezcolom@gmail.com>
2026-05-17 00:19:54 +0200

    Minor fixes.

--- demo/sites/redwood-oj/js/demo.js
+++ demo/sites/redwood-oj/js/demo.js
@@ -18,15 +18,8 @@ require([
                     console.log('add the ui, after all dependencies have been loaded');
 
                         const container = document.getElementById("ui-container");
-                        /*
-                        <mateu-ui baseUrl="/fluent"
-                config='{"tenantId": "1111","profile": "dev"}'
-                style="display: block; width: 100%; height: calc(100vh - 100px);"
-      >
-      </mateu-ui>
-                         */
                         const ui = document.createElement('mateu-ui');
-                        ui.setAttribute("baseUrl", "https://demo.mateu.io/fluent");
+                        ui.setAttribute("baseUrl", "https://demo.mateu.io");
                         ui.setAttribute("config", '{"tenantId": "1111","profile": "dev"}');
                         ui.setAttribute("style", "display: block; width: 100%; height: calc(100vh - 100px);");
                         container?.appendChild(ui);
