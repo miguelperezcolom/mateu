bbdc6ee09fd0a6edc2ce2197a2e0ba840ed3f548
Miguel <miguelperezcolom@gmail.com>
2026-06-04 14:33:12 +0200

    wip.

--- backend/shared/core/src/main/java/io/mateu/core/domain/act/CodeSearchFieldActionRunner.java
+++ backend/shared/core/src/main/java/io/mateu/core/domain/act/CodeSearchFieldActionRunner.java
@@ -1,14 +1,11 @@
 package io.mateu.core.domain.act;
 
 import io.mateu.core.application.runaction.RunActionCommand;
-import io.mateu.core.domain.out.componentmapper.ReflectionComponentMapper;
 import io.mateu.uidl.annotations.Searchable;
-import io.mateu.uidl.data.Data;
 import io.mateu.uidl.data.Dialog;
 import io.mateu.uidl.data.ServerSideComponent;
 import io.mateu.uidl.fluent.Component;
 import io.mateu.uidl.interfaces.HttpRequest;
-import io.mateu.uidl.interfaces.LabelSupplier;
 import io.mateu.uidl.interfaces.Selector;
 import jakarta.inject.Named;
 import lombok.SneakyThrows;
@@ -16,22 +13,16 @@ import reactor.core.publisher.Flux;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import static io.mateu.core.application.runaction.ComponentStateHelper.getState;
 import static io.mateu.core.application.runaction.RunActionUseCase.wrap;
 import static io.mateu.core.domain.act.FieldCrudActionRunner.getViewModelClass;
 import static io.mateu.core.domain.out.fragmentmapper.mappers.ActionMapper.createActions;
-import static io.mateu.core.domain.out.fragmentmapper.mappers.ActionMapper.mapActions;
 import static io.mateu.core.domain.out.fragmentmapper.mappers.FutureComponentMapper.createComponent;
 import static io.mateu.core.domain.out.fragmentmapper.mappers.RuleMapper.createRules;
-import static io.mateu.core.domain.out.fragmentmapper.mappers.RuleMapper.mapRules;
 import static io.mateu.core.domain.out.fragmentmapper.mappers.TriggerMapper.createTriggers;
-import static io.mateu.core.domain.out.fragmentmapper.mappers.TriggerMapper.mapTriggers;
 import static io.mateu.core.domain.out.fragmentmapper.mappers.ValidationMapper.createValidations;
-import static io.mateu.core.domain.out.fragmentmapper.mappers.ValidationMapper.mapValidations;
-import static io.mateu.core.infra.declarative.orchestrators.crud.DataLayer.getLabelSupplier;
 import static io.mateu.core.infra.declarative.orchestrators.crud.DataLayer.getSelector;
 import static io.mateu.core.infra.reflection.read.FieldByNameProvider.getFieldByName;
 import static io.mateu.uidl.reflection.GenericClassProvider.getGenericClass;
@@ -82,7 +73,7 @@ public class CodeSearchFieldActionRunner implements ActionRunner {
       }
 
       return Flux.just(Dialog.builder()
-                      .content(wrapSSC(createComponent(optionsSupplier, command.baseUrl(), command.route(), command.consumedRoute(), command.initiatorComponentId(), command.httpRequest()),
+                      .content(wrapWithServerSideComponent(createComponent(optionsSupplier, command.baseUrl(), command.route(), command.consumedRoute(), command.initiatorComponentId(), command.httpRequest()),
                               optionsSupplier,
                               command.baseUrl(),
                               command.route(),
@@ -95,13 +86,13 @@ public class CodeSearchFieldActionRunner implements ActionRunner {
     return null;
   }
 
-    private Component wrapSSC(Component clientSideComponent,
-                              Object serverSideComponent,
-                              String baseUrl,
-                              String route,
-                              String consumedRoute,
-                              String initiatorComponentId,
-                              HttpRequest httpRequest
+    private Component wrapWithServerSideComponent(Component clientSideComponent,
+                                                  Object serverSideComponent,
+                                                  String baseUrl,
+                                                  String route,
+                                                  String consumedRoute,
+                                                  String initiatorComponentId,
+                                                  HttpRequest httpRequest
     ) {
       return ServerSideComponent.builder()
               .id(httpRequest.getAttribute("upstreamComponentId") != null
