503bc9eb867b540d8fd877cc99b2b3a61a9b37d6
Miguel <miguelperezcolom@gmail.com>
2026-06-15 08:06:21 +0200

    Fix filtered auto crud.

--- backend/shared/core/src/main/java/io/mateu/core/infra/declarative/orchestrators/crud/FilteredAutoCrudOrchestrator.java
+++ backend/shared/core/src/main/java/io/mateu/core/infra/declarative/orchestrators/crud/FilteredAutoCrudOrchestrator.java
@@ -1,17 +1,69 @@
 package io.mateu.core.infra.declarative.orchestrators.crud;
 
+import static io.mateu.core.infra.declarative.orchestrators.crud.CrudAdapterHelper.getIdField;
 
 import io.mateu.core.infra.declarative.SimpleView;
 import io.mateu.uidl.data.Pageable;
+import io.mateu.uidl.interfaces.CrudAdapter;
 import io.mateu.uidl.interfaces.HttpRequest;
 import io.mateu.uidl.interfaces.Identifiable;
 
-public abstract class FilteredAutoCrudOrchestrator<Filters, Row extends Identifiable>
-    extends CrudOrchestrator<
-        SimpleView<Row>, SimpleView<Row>, SimpleView<Row>, Filters, Row, String> {
+public abstract class FilteredAutoCrudOrchestrator<Filters, T extends Identifiable>
+    extends CrudOrchestrator<SimpleView<T>, SimpleView<T>, SimpleView<T>, Filters, T, String> {
 
   @Override
-  public final Object search(
+  public String toId(String id) {
+    return id;
+  }
+
+  @Override
+  public CrudAdapter<SimpleView<T>, SimpleView<T>, SimpleView<T>, Filters, T, String> adapter() {
+    return (CrudAdapter) simpleAdapter();
+  }
+
+  public abstract AutoCrudAdapter<T> simpleAdapter();
+
+  public Class filtersClass() {
+    return entityClass();
+  }
+
+  public Class rowClass() {
+    return entityClass();
+  }
+
+  public Class viewClass() {
+    return entityClass();
+  }
+
+  @Override
+  public Class editorClass() {
+    return entityClass();
+  }
+
+  @Override
+  public Class creationFormClass() {
+    return entityClass();
+  }
+
+  @Override
+  public Object save(HttpRequest httpRequest) {
+    var entity = simpleAdapter().toEntity(httpRequest);
+    simpleAdapter().getEditor(entity.id(), httpRequest).save(httpRequest);
+    return entity.id();
+  }
+
+  @Override
+  public Object saveNew(HttpRequest httpRequest) {
+    return simpleAdapter().getCreationForm(httpRequest).create(httpRequest);
+  }
+
+  @Override
+  public String getIdFieldForRow() {
+    return getIdField(entityClass());
+  }
+
+  @Override
+  public Object search(
       String searchText, Object filters, Pageable pageable, HttpRequest httpRequest) {
     return adapter().search(searchText, (Filters) filters, pageable, httpRequest);
   }
--- backend/shared/core/src/main/java/io/mateu/core/infra/declarative/orchestrators/crud/FilteredAutoListOrchestrator.java
+++ backend/shared/core/src/main/java/io/mateu/core/infra/declarative/orchestrators/crud/FilteredAutoListOrchestrator.java
@@ -1,18 +1,15 @@
 package io.mateu.core.infra.declarative.orchestrators.crud;
 
 import static io.mateu.core.infra.declarative.orchestrators.crud.CrudAdapterHelper.getIdField;
-import static io.mateu.uidl.reflection.GenericClassProvider.getGenericClass;
 
 import io.mateu.core.infra.declarative.SimpleView;
-import io.mateu.uidl.data.ListingData;
 import io.mateu.uidl.data.Pageable;
 import io.mateu.uidl.interfaces.CrudAdapter;
 import io.mateu.uidl.interfaces.HttpRequest;
 import io.mateu.uidl.interfaces.Identifiable;
 
-public abstract class FilteredAutoListOrchestrator<Filters, Row extends Identifiable>
-    extends CrudOrchestrator<
-        SimpleView<Row>, SimpleView<Row>, SimpleView<Row>, Filters, Row, String> {
+public abstract class FilteredAutoListOrchestrator<Filters, T extends Identifiable>
+    extends CrudOrchestrator<SimpleView<T>, SimpleView<T>, SimpleView<T>, Filters, T, String> {
 
   @Override
   public boolean readOnly() {
@@ -20,51 +17,37 @@ public abstract class FilteredAutoListOrchestrator<Filters, Row extends Identifi
   }
 
   @Override
-  public final String toId(String id) {
+  public String toId(String id) {
     return id;
   }
 
   @Override
+  public CrudAdapter<SimpleView<T>, SimpleView<T>, SimpleView<T>, Filters, T, String> adapter() {
+    return (CrudAdapter) simpleAdapter();
+  }
+
+  public abstract AutoListAdapter<T> simpleAdapter();
+
   public Class filtersClass() {
-    return getGenericClass(getClass(), FilteredAutoListOrchestrator.class, "Filters");
+    return entityClass();
   }
 
-  @Override
   public Class rowClass() {
-    return getGenericClass(getClass(), FilteredAutoListOrchestrator.class, "Row");
+    return entityClass();
   }
 
-  @Override
   public Class viewClass() {
-    return rowClass();
+    return entityClass();
   }
 
   @Override
   public Class editorClass() {
-    return rowClass();
+    return entityClass();
   }
 
   @Override
   public Class creationFormClass() {
-    return rowClass();
-  }
-
-  @Override
-  public final Object search(
-      String searchText, Object filters, Pageable pageable, HttpRequest httpRequest) {
-    return doSearch(searchText, (Filters) filters, pageable, httpRequest);
-  }
-
-  public abstract ListingData<Row> doSearch(
-      String searchText, Filters filters, Pageable pageable, HttpRequest httpRequest);
-
-  @Override
-  public abstract Object view(String id, HttpRequest httpRequest);
-
-  @Override
-  public final CrudAdapter<SimpleView<Row>, SimpleView<Row>, SimpleView<Row>, Filters, Row, String>
-      adapter() {
-    return (CrudAdapter) new FilteredAutoListAdapter<>(this);
+    return entityClass();
   }
 
   @Override
@@ -79,6 +62,12 @@ public abstract class FilteredAutoListOrchestrator<Filters, Row extends Identifi
 
   @Override
   public String getIdFieldForRow() {
-    return getIdField(rowClass());
+    return getIdField(entityClass());
+  }
+
+  @Override
+  public Object search(
+      String searchText, Object filters, Pageable pageable, HttpRequest httpRequest) {
+    return adapter().search(searchText, (Filters) filters, pageable, httpRequest);
   }
 }
