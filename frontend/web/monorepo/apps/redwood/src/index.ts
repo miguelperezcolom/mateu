941f3328caaa4b1ee7f1e9a3c1a54c4ffad3bbbe
miguelperezcolom <miguelperezcolom@gmail.com>
2026-06-27 04:05:14 +0200

    Remove redwood lit renderer.

--- frontend/web/monorepo/apps/redwood/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { componentRenderer } from '@infra/ui/renderers/ComponentRenderer'
-import { RedwoodComponentRenderer } from "./RedwoodComponentRenderer.ts";
-import '@infra/ui/mateu-ui'
-
-componentRenderer.set(new RedwoodComponentRenderer())
